65468967308c87802d7e92d7fa4955d57916249b
hare27 <hare27@icloud.com>
2017-12-06 14:33:36 +0800

    0. 一个用户播放文字的按钮

---
+++ ADemo/ADemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ADemo
+//
+//  Created by 张文军 on 2017/12/6.
+//  Copyright © 2017年 xbwqlwq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  speakingDemo
+//
+//  Created by 张文军 on 2017/12/5.
+//  Copyright © 2017年 xbwqlwq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
